d9e7a254863ca1a55d6e8efeb3c7a88c998a709c
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2021-12-01 16:31:11 +0200

    my first commit

---
+++ CSharpUdemyCourse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpUdemyCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
