d0144b90485558ebdd2d27e730ca93706ea0440b
Evgenia Chatzi <e.chatzi@kaizengaming.com>
2022-01-17 10:39:42 +0200

    adding some text exercises

---
+++ TextExercises/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextExercises
+{
+    class Program
+    {
+        public void Exercise1()
+        {
+            Console.Write("Enter numbers separated by hyphen : ");
+            var input = Console.ReadLine();
+
+
+            var numbers = new List<int>();
+            foreach (var number in input.Split("-"))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+            numbers.Sort();
+
+            var isConsecutive = true;
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1)
+                {
+                    isConsecutive = false;
+                    break;
+                }
+            }
+
+            var message = isConsecutive ? "Consecutive" : "not Consecutive";
+
+            Console.WriteLine(message);
+
+        }
+
+        static void Main()
+        {
+
+        }
+
+
+
+        
+    }
+}
